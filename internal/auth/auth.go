459590136e6441d4f7536fc2dbff596f45e02d9c
srcct3 <src.ct3@gmail.com>
2026-01-16 15:18:55 +0300

    add jwt authentication hellper functions

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,70 @@
 package auth
 
-import "github.com/alexedwards/argon2id"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+const TokenAccessType = "chirpy"
+
+var ErrNoAuthHeaderIncluded = errors.New("no auth header included in request")
+
+func GetBearerToken(header http.Header) (string, error) {
+	authHeader := header.Get("Authorization")
+	if authHeader == "" {
+		return "", ErrNoAuthHeaderIncluded
+	}
+
+	splitAuth := strings.Split(authHeader, " ")
+	if len(splitAuth) < 2 || splitAuth[0] != "Bearer" {
+		return "", errors.New("malformed authorization header")
+	}
+	return splitAuth[1], nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiersIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    TokenAccessType,
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiersIn)),
+		Subject:   userID.String(),
+	})
+	return token.SignedString([]byte(tokenSecret))
+}
+
+func ValidateJWT(headerString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(
+		headerString,
+		&jwt.RegisteredClaims{},
+		func(t *jwt.Token) (any, error) { return []byte(tokenSecret), nil },
+	)
+	if err != nil {
+		return uuid.Nil, err
+	}
+	subject, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	issuer, err := token.Claims.GetIssuer()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	if issuer != TokenAccessType {
+		return uuid.Nil, errors.New("Invalid issuer")
+	}
+	userID, err := uuid.Parse(subject)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("Invalid user ID: %w", err)
+	}
+	return userID, nil
+}
 
 func HashPassword(password string) (string, error) {
 	return argon2id.CreateHash(password, argon2id.DefaultParams)
